5351449bad17a8a7ba171047f769ebc712463003
SPIF KLA - Cedric Nageri <Cedric.Nageri@cevalogistics.com>
2022-10-31 11:07:50 +0300

    feat: connection2DB

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
+require("dotenv").config();
+const connectDB = require("./config/db");
 const app = express();
+
+// Extra security packages
+// const helmet = require("helmet");
+const cors = require("cors");
+// const xss = require("xss-clean");
+// const rateLimiter = require("express-rate-limit");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +17,14 @@ app.get("/", (req, res) => {
 app.use("/api/v0/users", require("./routes/users"));
 app.use("/api/v0/auth", require("./routes/auth"));
 app.use("/api/v0/contacts", require("./routes/contacts"));
+
+app.use(cors());
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  try {
+    connectDB(process.env.MONGODB_URI);
+    console.log(`Server is running on port ${PORT}`);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 });
